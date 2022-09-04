566a3b785dfd32bd343d86f0ba5c40ce7bdf754c
Lemain99 <102553300+Lemain99@users.noreply.github.com>
2022-09-04 16:35:39 +0300

    Added FileUploadController

---
+++ src/main/java/dev/compressor/FileUploadController.java
@@ -0,0 +1,33 @@
+package dev.compressor;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public  String handleFileUpload(@RequestParam("name") String name, @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "Вы удачно загрузили " + name + " в " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "Вам не удалось загрузить " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "Вам не удалось загрузить " + name + " потому что файл пустой.";
+        }
+    }
+
+}
