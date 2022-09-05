c10d67e2883c04408639c1790194f5a1f0451f27
Lemain99 <102553300+Lemain99@users.noreply.github.com>
2022-09-05 22:34:16 +0300

    Added file compressing

--- src/main/java/dev/compressor/endpoint/FileUploadController.java
+++ src/main/java/dev/compressor/endpoint/FileUploadController.java
@@ -4,6 +4,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import dev.compressor.service.CompressionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
+
+
+    @Autowired
+    private CompressionService compressionService;
+
+
+
     @RequestMapping(value="/upload", method=RequestMethod.POST)
     public  String handleFileUpload(@RequestParam("name") String name, @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
             try {
+                compressionService.compressFile(file,name);
                 byte[] bytes = file.getBytes();
                 BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
                 stream.write(bytes);
--- src/main/java/dev/compressor/service/CompressionService.java
+++ src/main/java/dev/compressor/service/CompressionService.java
@@ -3,20 +3,25 @@ package dev.compressor.service;
 import dev.compressor.util.Compressor;
 import dev.compressor.util.SVGCompressor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 @Slf4j
+@Service
 public class CompressionService {
-    File compressFile(MultipartFile file){
+    public File compressFile(MultipartFile file, String inputFileName){
         Compressor compressor = new SVGCompressor();
         try {
            byte[] result = compressor.compress(file.getBytes());
-            Files.write(Path.of("/tmp/new"),result);
+           String outputFileName = inputFileName +"_compressed.svg";
+            Files.write(Path.of("/tmp/" + outputFileName ),result, StandardOpenOption.CREATE);
+
         } catch (IOException e) {
             log.error("Cannot read bytes from file. {}",e.getMessage());
             throw new RuntimeException(e);
